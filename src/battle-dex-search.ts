f10d96c6b52db3d4c8cb5a1398cc8c4100d32f2a
Maribel Hearn <maribelhearn@disroot.org>
2024-10-25 22:35:28 +0200

    Test slice thingy

--- src/battle-dex-search.ts
+++ src/battle-dex-search.ts
@@ -939,10 +939,10 @@ class BattlePokemonSearch extends BattleTypedSearch<'pokemon'> {
 		else if (format === 'pu') tierSet = tierSet.slice(slices.PU || slices.NU);
 		else if (format === 'zu') tierSet = tierSet.slice(slices.ZU || slices.PU || slices.NU);
 		else if (format === 'lc' || format === 'lcuu' || format.startsWith('lc') || (format !== 'caplc' && format.endsWith('lc'))) tierSet = tierSet.slice(slices.LC);
-		else if (format === 'fundex') tierSet = tierSet.slice(0, slices.AG || slices.Uber);
+		else if (format === 'fundex') tierSet = tierSet.slice(0, slices.CAP);
 		else if (format === 'nationalfundex') tierSet = tierSet.slice(0, slices.AG || slices.Uber).concat(tierSet.slice(slices.OU));
 		else if (format === 'fundexpurehackmons') tierSet = tierSet.slice(0, slices.AG || slices.Uber).concat(tierSet.slice(slices.OU));
-		else if (format === 'cap') tierSet = tierSet.slice(0, slices.AG || slices.Uber).concat(tierSet.slice(slices.OU));
+		else if (format === 'cap') tierSet = tierSet.slice(slices.Fundex, slices.AG || slices.Uber).concat(tierSet.slice(slices.OU));
 		else if (format === 'caplc') {
 			tierSet = tierSet.slice(slices['CAP LC'], slices.AG || slices.Uber).concat(tierSet.slice(slices.LC));
 		} else if (format === 'anythinggoes' || format.endsWith('ag') || format.startsWith('ag')) {
