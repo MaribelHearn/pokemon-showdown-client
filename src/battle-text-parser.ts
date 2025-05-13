600d0999fe6716b828487f07d67616c4ee1686a2
Maribel Hearn <maribelhearn@disroot.org>
2025-05-13 20:40:30 +0200

    Fix Giygas move message

--- src/battle-text-parser.ts
+++ src/battle-text-parser.ts
@@ -540,7 +540,7 @@ class BattleTextParser {
 			if (kwArgs.zeffect) {
 				line1 = this.template('zEffect').replace('[POKEMON]', this.pokemon(pokemon));
 			}
-			if (this.pokemon(pokemon).toLowerCase() == 'the opposing Giygas') {
+			if (this.pokemon(pokemon).toLowerCase() == 'the opposing giygas') {
 				const template = this.template('moveGiygas', kwArgs.from);
 				return line1 + template;
 			}
