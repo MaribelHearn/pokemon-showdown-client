3119255d524100f52f7b0ff5e2dd469bca9c8cdd
Maribel Hearn <maribelhearn@disroot.org>
2025-08-10 00:55:32 +0200

    Sledge without above()

--- src/battle-animations-moves.ts
+++ src/battle-animations-moves.ts
@@ -35712,17 +35712,17 @@ BattleMoveAnims['sledge'] = {
 	anim(scene, [attacker, defender]) {
 		scene.showEffect('sledge', {
 			x: defender.x,
-			y: defender.above(200),
+			y: defender.y + 200,
 			z: defender.z,
 		}, {
 			x: defender.x,
-			y: defender.above(50),
+			y: defender.y + 50,
 			z: defender.z,
 			time: 400,
 		}, 'linear', 'fade');
 		scene.showEffect('sledge', {
 			x: defender.x,
-			y: defender.above(50),
+			y: defender.y + 50,
 			z: defender.z,
 			time: 400,
 		}, {
