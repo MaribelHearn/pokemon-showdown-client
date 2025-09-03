f3d39cf3887252c8b05131058396bbe9f35fcdfa
Maribel Hearn <maribelhearn@disroot.org>
2025-09-03 23:53:09 +0200

    Smithy tooltip test

--- src/battle-tooltips.ts
+++ src/battle-tooltips.ts
@@ -810,10 +810,10 @@ class BattleTooltips {
 				if (clientPokemon.volatiles.transform) {
 					text += `<small>(Transformed into ${clientPokemon.volatiles.formechange[1]})</small><br />`;
 				} else {
-					if (clientPokemon.speciesForme == 'Smithy-Tank Head' && clientPokemon.shiny) {
+					if (clientPokemon.speciesForme === 'smithytankhead' && clientPokemon.shiny) {
 						text += `<small>(Changed forme: Smithy-Mask Head)</small><br />`;
 					}
-					else if (clientPokemon.speciesForme == 'Smithy-Magic Head' && clientPokemon.shiny) {
+					else if (clientPokemon.speciesForme === 'smithymagichead' && clientPokemon.shiny) {
 						text += `<small>(Changed forme: Smithy-Treasure Head)</small><br />`;
 					}
 					else {
