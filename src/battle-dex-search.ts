a22f68dbad192882f574fee0d17dac2e05e02a02
Maribel Hearn <maribelhearn@disroot.org>
2025-09-19 23:14:39 +0200

    Remove isMega console log

--- src/battle-dex-search.ts
+++ src/battle-dex-search.ts
@@ -1081,7 +1081,6 @@ class BattleAbilitySearch extends BattleTypedSearch<'ability'> {
 		let species = dex.species.get(this.species);
 		let abilitySet: SearchRow[] = [['header', "Abilities"]];
 
-		console.log(species.isMega);
 		if (species.isMega) {
 			abilitySet.unshift(['html', `Will be <strong>${species.abilities['0']}</strong> after Mega Evolving.`]);
 			species = dex.species.get(species.baseSpecies);
