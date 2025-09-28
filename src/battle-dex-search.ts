eb71116ec5c717b02a1741e731d928373d2d5fee
Maribel Hearn <maribelhearn@disroot.org>
2025-09-28 23:38:09 +0200

    Update slices

--- src/battle-dex-search.ts
+++ src/battle-dex-search.ts
@@ -952,7 +952,7 @@ class BattlePokemonSearch extends BattleTypedSearch<'pokemon'> {
 		else if (format === 'lc' || format === 'lcuu' || format.startsWith('lc') || (format !== 'caplc' && format !== 'fundexlc' && format.endsWith('lc'))) tierSet = tierSet.slice(slices.LC);
 		else if (format === 'nationalfundex') tierSet = tierSet.slice(0, slices.CAP || slices.Uber).concat(tierSet.slice(slices.OU));
 		else if (format === 'fundexpurehackmons') tierSet = tierSet.slice(0, slices.CAP).concat(tierSet.slice(slices.AG));
-		else if (format === 'fundexuber') tierSet = tierSet.slice(0, slices.CAP);
+		else if (format === 'fundexuber' || format == 'fundexmetronome') tierSet = tierSet.slice(0, slices.CAP);
 		else if (format === 'fundexlc') tierSet = tierSet.slice(slices['Fundex LC'], slices.CAP);
 		else if (format.startsWith('fundex')) tierSet = tierSet.slice(slices['Fundex OU'], slices.CAP);
 		else if (format === 'cap') tierSet = tierSet.slice(slices.CAP, slices.AG || slices.Uber).concat(tierSet.slice(slices.OU));
