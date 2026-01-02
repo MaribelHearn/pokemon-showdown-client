00b43cfa94c5053a25a1df9c976acc4f1b1c29a6
Maribel Hearn <maribelhearn@disroot.org>
2026-01-02 17:14:26 +0100

    Replay embed URL

--- js/replay-embed.template.js
+++ js/replay-embed.template.js
@@ -28,27 +28,27 @@ function requireScript(url) {
 	document.head.appendChild(scriptEl);
 }
 
-linkStyle('https://play.pokemonshowdown.com/style/font-awesome.css?');
-linkStyle('https://play.pokemonshowdown.com/style/battle.css?a7');
-linkStyle('https://play.pokemonshowdown.com/style/replay.css?a7');
-linkStyle('https://play.pokemonshowdown.com/style/utilichart.css?a7');
+linkStyle('https://ps.maribelhearn.com/style/font-awesome.css?');
+linkStyle('https://ps.maribelhearn.com/style/battle.css?a7');
+linkStyle('https://ps.maribelhearn.com/style/replay.css?a7');
+linkStyle('https://ps.maribelhearn.com/style/utilichart.css?a7');
 
-requireScript('https://play.pokemonshowdown.com/js/lib/ps-polyfill.js');
-requireScript('https://play.pokemonshowdown.com/config/config.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/lib/jquery-1.11.0.min.js');
-requireScript('https://play.pokemonshowdown.com/js/lib/html-sanitizer-minified.js');
-requireScript('https://play.pokemonshowdown.com/js/battle-sound.js');
-requireScript('https://play.pokemonshowdown.com/js/battledata.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/pokedex-mini.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/pokedex-mini-bw.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/graphics.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/pokedex.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/moves.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/abilities.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/items.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/teambuilder-tables.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/battle-tooltips.js?a7');
-requireScript('https://play.pokemonshowdown.com/js/battle.js?a7');
+requireScript('https://ps.maribelhearn.com/js/lib/ps-polyfill.js');
+requireScript('https://ps.maribelhearn.com/config/config.js?a7');
+requireScript('https://ps.maribelhearn.com/js/lib/jquery-1.11.0.min.js');
+requireScript('https://ps.maribelhearn.com/js/lib/html-sanitizer-minified.js');
+requireScript('https://ps.maribelhearn.com/js/battle-sound.js');
+requireScript('https://ps.maribelhearn.com/js/battledata.js?a7');
+requireScript('https://ps.maribelhearn.com/data/pokedex-mini.js?a7');
+requireScript('https://ps.maribelhearn.com/data/pokedex-mini-bw.js?a7');
+requireScript('https://ps.maribelhearn.com/data/graphics.js?a7');
+requireScript('https://ps.maribelhearn.com/data/pokedex.js?a7');
+requireScript('https://ps.maribelhearn.com/data/moves.js?a7');
+requireScript('https://ps.maribelhearn.com/data/abilities.js?a7');
+requireScript('https://ps.maribelhearn.com/data/items.js?a7');
+requireScript('https://ps.maribelhearn.com/data/teambuilder-tables.js?a7');
+requireScript('https://ps.maribelhearn.com/js/battle-tooltips.js?a7');
+requireScript('https://ps.maribelhearn.com/js/battle.js?a7');
 
 var Replays = {
 	battle: null,
