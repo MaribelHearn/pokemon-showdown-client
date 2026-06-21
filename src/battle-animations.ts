2c04d9d5514a3497b2127c47ee899491185b994a
Maribel Hearn <maribelhearn@disroot.org>
2026-06-21 20:51:31 +0200

    Yoda faint cry

--- src/battle-animations.ts
+++ src/battle-animations.ts
@@ -2643,7 +2643,7 @@ export class PokemonSprite extends Sprite {
 			// Fundex: faint cries
 			const shinyCry = ['Ganondorf'];
 
-			const faintCries = ['Baby Mario', 'Bloody Jitterbug', 'Bowser', 'Captain Falcon', 'Dark Matter', 'Darth Vader', 'Giygas', 'Kirby', 'Leeroy Jenkins', 'Link', 'Snake-Busted', 'Tabuu', 'Young Link', 'Zero'];
+			const faintCries = ['Baby Mario', 'Bloody Jitterbug', 'Bowser', 'Captain Falcon', 'Dark Matter', 'Yoda', 'Darth Vader', 'Giygas', 'Kirby', 'Leeroy Jenkins', 'Link', 'Snake-Busted', 'Tabuu', 'Young Link', 'Zero'];
 
 			const formeCries = ['Missingno.-Yellow'];
 
